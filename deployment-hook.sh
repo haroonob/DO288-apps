d1f57a6e498a0d9e20dae670aaabd6028892f690
Student User <student@workstation.lab.example.com>
2022-08-24 12:51:15 -0400

    deployment hook

---
+++ deployment-hook.sh
@@ -0,0 +1,1 @@
+oc set deployment-hook dc/mysql --post --failure-policy=abort -- /bin/sh,-c,curl -L -s https://github.com/RedHatTraining/DO288-apps/releases/download/OCP-4.1-1/import.sh -o /tmp/import.sh&&chmod 755 /tmp/import.sh&&/tmp/import.sh
